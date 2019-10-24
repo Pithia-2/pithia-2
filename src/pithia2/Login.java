94b9ce4fb7cd62dd4e14072d18c3d20c7af5e2b6
feedmycat <konstantangelou@gmail.com>
2019-10-24 21:40:41 +0300

    added Login form

---
+++ src/pithia2/Login.java
@@ -0,0 +1,6 @@
+package pithia2;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+}
--- src/pithia2/main.java
+++ src/pithia2/main.java
@@ -2,6 +2,7 @@ package pithia2;
 
 public class main {
     public static void main(String[] args) {
-
+        Login login = new Login();
+        login.setVisible(true);
     }
 }
