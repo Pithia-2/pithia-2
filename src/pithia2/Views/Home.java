985a83fb05b81d5a84194a23b085c3aa625c99e3
Stefanos Papanatsios <spapanatsios@gmail.com>
2019-11-02 20:58:18 +0200

    Redesigned home page

--- src/pithia2/Views/Home.java
+++ src/pithia2/Views/Home.java
@@ -2,6 +2,8 @@ package pithia2.Views;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,9 +15,13 @@ public class Home extends JFrame {
 
   private JPanel MainPanel;
   private JLabel Logo;
-  private JLabel Tiile;
+  private JLabel Title;
   private JButton LoginButton;
+  private JPanel Navbar;
   private JLabel Details;
+  private JButton DepartmentsButton;
+  private JButton LessonsButton;
+  private JLabel AppNameLabel;
 
   public Home() {
     add(MainPanel);
@@ -24,8 +30,15 @@ public class Home extends JFrame {
     setLocationRelativeTo(null);
     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     Details.setText(
-        "<html>Το Διεθνές Πανεπιστήμιο της Ελλάδος (ΔΙ.ΠΑ.Ε.) ιδρύθηκε το 2005 με έδρα την Θεσσαλονίκη.<br>"
-            + "Το πανεπιστήμιο αποτελείται απο 7 σχολές σε Θεσσαλονίκη, Δράμα, Καβάλα και Σέρρες.</html>");
+        "<html>Το Διεθνές Πανεπιστήμιο της Ελλάδος (ΔΙ.ΠΑ.Ε.) ιδρύθηκε το 2005 με έδρα την Θεσσαλονίκη. "
+            + "Το πανεπιστήμιο αποτελείται απο 7 σχολές σε Θεσσαλονίκη, Δράμα, Καβάλα και Σέρρες.</html>"
+    );
+
+    this.addWindowListener(new WindowAdapter() {
+      public void windowOpened(WindowEvent e) {
+        LoginButton.requestFocus();
+      }
+    });
 
     LoginButton.addActionListener(new ActionListener() {
       @Override
