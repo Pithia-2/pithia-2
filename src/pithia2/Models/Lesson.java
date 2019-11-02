ae66ff6ea93450610369f65d2e994448fb0be673
feedmycat <konstantangelou@gmail.com>
2019-11-02 14:51:50 +0200

    Added Lesson model

---
+++ src/pithia2/Models/Lesson.java
@@ -0,0 +1,78 @@
+package pithia2.Models;
+
+public class Lesson {
+  private int id;
+  private String name;
+  private int semester;
+  private int labHours;
+  private int theoryHours;
+  private int credit;
+  private String type;
+
+  public Lesson(int id, String name, int semester, int labHours, int theoryHours, int credit,
+      String type) {
+    this.id = id;
+    this.name = name;
+    this.semester = semester;
+    this.labHours = labHours;
+    this.theoryHours = theoryHours;
+    this.credit = credit;
+    this.type = type;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getSemester() {
+    return semester;
+  }
+
+  public void setSemester(int semester) {
+    this.semester = semester;
+  }
+
+  public int getLabHours() {
+    return labHours;
+  }
+
+  public void setLabHours(int labHours) {
+    this.labHours = labHours;
+  }
+
+  public int getTheoryHours() {
+    return theoryHours;
+  }
+
+  public void setTheoryHours(int theoryHours) {
+    this.theoryHours = theoryHours;
+  }
+
+  public int getCredit() {
+    return credit;
+  }
+
+  public void setCredit(int credit) {
+    this.credit = credit;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+}
