15c650ae6ef6f36998bae7800f079b2f1aca75a2
feedmycat <konstantangelou@gmail.com>
2019-11-02 15:17:27 +0200

    Added RegisteredLesson and Registration models

---
+++ src/pithia2/Models/Registration.java
@@ -0,0 +1,27 @@
+package pithia2.Models;
+
+public class Registration {
+
+  private int id;
+  private RegisteredLesson[] registeredLessons = new RegisteredLesson[9];
+
+  public Registration(int id) {
+    this.id = id;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public RegisteredLesson[] getRegisteredLessons() {
+    return registeredLessons;
+  }
+
+  public void setRegisteredLessons(RegisteredLesson[] registeredLessons) {
+    this.registeredLessons = registeredLessons;
+  }
+}
