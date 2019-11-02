ae6dba26ceed9210c698707b829d992b41dcccea
feedmycat <konstantangelou@gmail.com>
2019-11-02 14:40:56 +0200

    Added Department model

---
+++ src/pithia2/Models/Department.java
@@ -0,0 +1,28 @@
+package pithia2.Models;
+
+public class Department {
+
+  private String name;
+  private int phoneNumber;
+
+  public Department(String name, int phoneNumber) {
+    this.name = name;
+    this.phoneNumber = phoneNumber;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(int phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+}
