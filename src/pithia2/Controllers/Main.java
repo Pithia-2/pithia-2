d672488dd95d16297917db7ac3f1666d3ac3f95d
Stefanos Papanatsios <spapanatsios@gmail.com>
2019-11-02 18:47:53 +0200

    Redesigned login page - App now gets look and feel from user's OS

--- src/pithia2/Controllers/Main.java
+++ src/pithia2/Controllers/Main.java
@@ -2,18 +2,19 @@ package pithia2.Controllers;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import pithia2.Views.Home;
 
 public class Main {
 
-  public static void main(String[] args) {
-    SwingUtilities.invokeLater(new Runnable() {
-      @Override
-      public void run() {
-        JFrame frame = new Home();
-        frame.setVisible(true);
-      }
+  public static void main(String[] args)
+      throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+    SwingUtilities.invokeLater(() -> {
+      JFrame frame = new Home();
+      frame.setVisible(true);
     });
   }
-
 }
