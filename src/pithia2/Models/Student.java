8f95185b46b80b78c42a640429f544fe78460be0
feedmycat <konstantangelou@gmail.com>
2019-11-02 15:50:25 +0200

    Changed Student to fit the new model

--- src/pithia2/Models/Student.java
+++ src/pithia2/Models/Student.java
@@ -3,11 +3,16 @@ package pithia2.Models;
 public class Student extends User {
 
   private int studentCode;
+  private String department;
+  private int semester;
+  private Lesson[] passedLessons = new Lesson[100];
 
-  public Student(String username, String firstname, String lastname, String department,
-      int semester, int studentCode) {
-    super(username, firstname, lastname, department);
+  public Student(String username, String password, String fullname, String email, int studentCode,
+      String department, int semester) {
+    super(username, password, fullname, email);
     this.studentCode = studentCode;
+    this.department = department;
+    this.semester = semester;
   }
 
   public int getStudentCode() {
@@ -16,5 +21,29 @@ public class Student extends User {
 
   public void setStudentCode(int studentCode) {
     this.studentCode = studentCode;
+  }
+
+  public String getDepartment() {
+    return department;
+  }
+
+  public void setDepartment(String department) {
+    this.department = department;
+  }
+
+  public int getSemester() {
+    return semester;
+  }
+
+  public void setSemester(int semester) {
+    this.semester = semester;
+  }
+
+  public Lesson[] getPassedLessons() {
+    return passedLessons;
+  }
+
+  public void setPassedLessons(Lesson[] passedLessons) {
+    this.passedLessons = passedLessons;
   }
 }
