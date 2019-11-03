faa06b3b5b9aff22624060c258af7a4f115cdcbd
feedmycat <konstantangelou@gmail.com>
2019-11-03 19:01:41 +0200

    Added global variables for objects and root. Closes #20

--- src/pithia2/GlobalConstants.java
+++ src/pithia2/GlobalConstants.java
@@ -1,6 +1,19 @@
 package pithia2;
 
+import java.nio.file.Paths;
+
 public class GlobalConstants {
+
   public static final int FRAME_WIDTH = 1280;
   public static final int FRAME_HEIGHT = 720;
+
+  public static final String APP_ROOT = Paths.get(".").toAbsolutePath().normalize().toString();
+  public static final String STUDENTS_PATH = APP_ROOT + "\\Objects\\Students\\";
+  public static final String ADMIN_PATH = APP_ROOT + "\\Objects\\Administrators\\";
+  public static final String UNIVERSITY_PATH = APP_ROOT + "\\Objects\\Universities\\";
+  public static final String DEPARTMENTS_PATH = APP_ROOT + "\\Objects\\Departments\\";
+  public static final String LESSONS_PATH = APP_ROOT + "\\Objects\\Lessons\\";
+  public static final String REGISTEREDLESSONS_PATH = APP_ROOT + "\\Objects\\RegisteredLessons\\";
+  public static final String REGISTRATIONS_PATH = APP_ROOT + "\\Objects\\Registrations\\";
+
 }
