4517678b51611b0f2c106322154cec90c11818ff
feedmycat <konstantangelou@gmail.com>
2019-11-07 17:00:04 +0200

    changed object Arrays to be ArrayLists

--- src/pithia2/Models/Registration.java
+++ src/pithia2/Models/Registration.java
@@ -1,11 +1,13 @@
 package pithia2.Models;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Registration implements Serializable {
 
   private int id;
-  private RegisteredLesson[] registeredLessons = new RegisteredLesson[9];
+  private List<RegisteredLesson> registeredLessons = new ArrayList<RegisteredLesson>();
 
   public Registration(int id) {
     this.id = id;
@@ -15,11 +17,11 @@ public class Registration implements Serializable {
 
   public void setId(int id) { this.id = id; }
 
-  public RegisteredLesson[] getRegisteredLessons() {
+  public List<RegisteredLesson> getRegisteredLessons() {
     return registeredLessons;
   }
 
-  public void setRegisteredLessons(RegisteredLesson[] registeredLessons) {
+  public void setRegisteredLessons(List<RegisteredLesson> registeredLessons) {
     this.registeredLessons = registeredLessons;
   }
 }
