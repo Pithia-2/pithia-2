2d573e692bdeb8b25e310ddfaad41aa8bd7b9583
Stefanos Papanatsios <spapanatsios@gmail.com>
2019-11-09 19:54:26 +0200

    Added AdminGrades page

---
+++ src/pithia2/Views/AdminGrades.java
@@ -0,0 +1,41 @@
+package pithia2.Views;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.WindowConstants;
+import pithia2.GlobalConstants;
+
+public class AdminGrades extends JFrame {
+
+  private JPanel RootPanel;
+  private JPanel Navbar;
+  private JButton HomeButton;
+  private JButton SignoutButton;
+  private JTable StudentTable;
+  private JButton ConfirmButton;
+  private JTable LessonTable;
+  private JPanel TablePanel;
+  private JPanel ConfirmPanel;
+
+  AdminGrades() {
+    add(RootPanel);
+    setSize(GlobalConstants.FRAME_WIDTH, GlobalConstants.FRAME_HEIGHT);
+    setResizable(false);
+    setLocationRelativeTo(null);
+    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    HomeButton.addActionListener(e -> {
+      AdminHome ah = new AdminHome();
+      ah.setVisible(true);
+      dispose();
+    });
+
+    SignoutButton.addActionListener(e -> {
+      Login login = new Login();
+      login.setVisible(true);
+      dispose();
+    });
+  }
+}
