8464e7a746c275566437daf2570263bde7a3f241
FeedMyCat54 <konstantangelou@gmail.com>
2019-11-09 14:46:49 +0200

    Reads a university object file and creates an instance.

--- src/pithia2/Controllers/Main.java
+++ src/pithia2/Controllers/Main.java
@@ -1,15 +1,32 @@
 package pithia2.Controllers;
 
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+import pithia2.GlobalConstants;
+import pithia2.Models.University;
 import pithia2.Views.Home;
+
+import javax.swing.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 public class Main {
 
   public static void main(String[] args)
-      throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+    throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
     UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+    String path = GlobalConstants.UNIVERSITIES_PATH + "test.univer";
+    try {
+      ObjectInputStream is = new ObjectInputStream(new FileInputStream(path));
+
+      University.setUniversityInstance((University) is.readObject());
+      is.close();
+    } catch (FileNotFoundException e) {
+      System.out.println(e.getMessage());
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
 
     SwingUtilities.invokeLater(() -> {
       Home home = new Home();
