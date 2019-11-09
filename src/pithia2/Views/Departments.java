83630f4f0b7b0df2f0250cf96afdec3a748a9f77
Stefanos Papanatsios <spapanatsios@gmail.com>
2019-11-09 14:38:15 +0200

    Renamed DepartmentPage to Departments, beautified page and code, added error handling, removed unused sign out button and fixed bugs

---
+++ src/pithia2/Views/Departments.java
@@ -0,0 +1,58 @@
+package pithia2.Views;
+
+import java.util.List;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.WindowConstants;
+import javax.swing.table.DefaultTableModel;
+import pithia2.GlobalConstants;
+import pithia2.Models.Department;
+import pithia2.Models.University;
+
+public class Departments extends JFrame {
+
+  private JPanel DepartmentPanel;
+  private JButton HomeButton;
+  private JTable DepartmentTable;
+  private JPanel Navbar;
+  private JLabel errorLabel;
+
+  Departments() {
+    add(DepartmentPanel);
+    setSize(GlobalConstants.FRAME_WIDTH, GlobalConstants.FRAME_HEIGHT);
+    setResizable(false);
+    setLocationRelativeTo(null);
+    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    HomeButton.addActionListener(e -> {
+      Home home = new Home();
+      home.setVisible(true);
+      dispose();
+    });
+
+    ListDepartments();
+  }
+
+   private void ListDepartments() {
+    if (University.getUserInstance() != null) {
+      List<Department> departments = University.getUserInstance().getDepartments();
+      DefaultTableModel model = (DefaultTableModel) DepartmentTable.getModel();
+      model.addColumn("Name");
+      model.addColumn("Phone Number");
+
+      Object[] rowData = new Object[2];
+      for (Department department : departments) {
+        rowData[0] = department.getName();
+        rowData[1] = department.getPhoneNumber();
+        model.addRow(rowData);
+      }
+
+      DepartmentTable.setModel(model);
+    } else {
+      errorLabel.setText("No data found.");
+    }
+  }
+}
