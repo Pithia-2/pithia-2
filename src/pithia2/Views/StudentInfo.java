225517e8259ec97b9424e269764c385427d00c92
feedmycat <konstantangelou@gmail.com>
2019-11-10 17:35:10 +0200

    fixed department import missing

--- src/pithia2/Views/StudentInfo.java
+++ src/pithia2/Views/StudentInfo.java
@@ -7,6 +7,7 @@ import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 import pithia2.GlobalConstants;
 import pithia2.Models.Student;
+import pithia2.Models.Department;
 
 public class StudentInfo extends JFrame {
 
