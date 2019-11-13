1deb2a30d82630be6b955e4787ddc200697c8c4c
Stefanos Papanatsios <spapanatsios@gmail.com>
2019-11-13 21:36:17 +0200

    Reworked UserManagement UI, added code to generate table data, buttons to create new student and admin - Added save to file global function

--- src/pithia2/GlobalConstants.java
+++ src/pithia2/GlobalConstants.java
@@ -1,6 +1,10 @@
 package pithia2;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.nio.file.Paths;
+import pithia2.Models.University;
 
 public class GlobalConstants {
 
@@ -9,4 +13,15 @@ public class GlobalConstants {
 
   public static final String APP_ROOT = Paths.get(".").toAbsolutePath().normalize().toString();
   public static final String UNIVERSITIES_PATH = APP_ROOT + "\\Universities\\";
+
+  public static void save() {
+    String path = UNIVERSITIES_PATH + "test.uni";
+    try {
+      ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(path));
+      os.writeObject(University.getUniversityInstance());
+      os.close();
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+  }
 }
