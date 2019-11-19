23e7f44c074b0c2d6206064634cd9ab3301ac979
Stefanos Papanatsios <spapanatsios@gmail.com>
2019-11-19 22:13:56 +0200

    Added code to show students and their registrations

--- src/pithia2/Views/AdminGrades.java
+++ src/pithia2/Views/AdminGrades.java
@@ -1,12 +1,22 @@
 package pithia2.Views;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.WindowConstants;
+import javax.swing.table.DefaultTableModel;
 import pithia2.GlobalConstants;
 import pithia2.Models.Administrator;
+import pithia2.Models.RegisteredLesson;
+import pithia2.Models.Registration;
+import pithia2.Models.Student;
+import pithia2.Models.University;
+import pithia2.Models.User;
 
 public class AdminGrades extends JFrame {
 
@@ -20,6 +30,8 @@ public class AdminGrades extends JFrame {
   private JTable LessonTable;
   private JPanel TablePanel;
   private JPanel ConfirmPanel;
+  private JScrollPane Lessons;
+  private JScrollPane Students;
 
   AdminGrades() {
     add(RootPanel);
@@ -46,5 +58,72 @@ public class AdminGrades extends JFrame {
       dispose();
       Administrator.getAdminInstance().logout();
     });
+
+    StudentTable.addMouseListener(new MouseAdapter() {
+      @Override
+      public void mouseClicked(MouseEvent mouseEvent) {
+        super.mouseClicked(mouseEvent);
+        loadLessons();
+      }
+    });
+  }
+
+  private void createUIComponents() {
+    String[] studentTableColumns = {"Student Code", "Name"};
+    DefaultTableModel studentTableModel = new DefaultTableModel(studentTableColumns, 0);
+    StudentTable = new JTable(studentTableModel);
+    StudentTable.setDefaultEditor(Object.class, null);
+
+    String[] lessonTableColumns = {"ID", "Name", "Grade"};
+    DefaultTableModel lessonTableModel = new DefaultTableModel(lessonTableColumns, 0);
+    LessonTable = new JTable(lessonTableModel);
+
+    loadStudents();
+  }
+
+  private void loadStudents() {
+    List<User> users = University.getUniversityInstance().getUsers();
+    for (User user : users) {
+      if (user instanceof Student) {
+        Student student = (Student) user;
+        Object[] row = new Object[2];
+        row[0] = student.getStudentCode();
+        row[1] = student.getFullname();
+
+        ((DefaultTableModel) StudentTable.getModel()).addRow(row);
+      }
+    }
+  }
+
+  private void loadLessons() {
+    DefaultTableModel lessonTableModel = (DefaultTableModel) LessonTable.getModel();
+    lessonTableModel.setRowCount(0);
+
+    int studentTableSelectedRow = StudentTable.getSelectedRow();
+    int studentCode = Integer.parseInt(StudentTable.getValueAt(studentTableSelectedRow, 0).toString());
+    List<User> users = University.getUniversityInstance().getUsers();
+    Student selectedStudent;
+
+    for (User user : users) {
+      if (user instanceof Student && ((Student) user).getStudentCode() == studentCode) {
+        selectedStudent = (Student) user;
+
+        int registrationCount = selectedStudent.getRegistrations().size();
+        if (registrationCount > 0) {
+          int lastRegistrationIndex = registrationCount - 1;
+          Registration lastRegistration = selectedStudent.getRegistrations().get(lastRegistrationIndex);
+          List<RegisteredLesson> registeredLessons = lastRegistration.getRegisteredLessons();
+
+          for (RegisteredLesson registeredLesson : registeredLessons) {
+            Object[] row = new Object[3];
+            row[0] = registeredLesson.getId();
+            row[1] = registeredLesson.getName();
+            row[2] = registeredLesson.getGrade();
+
+            ((DefaultTableModel) LessonTable.getModel()).addRow(row);
+          }
+        }
+      }
+    }
   }
 }
