2bbd5879d563566f96b0d1a3eefca9e5e27ce5ea
Stefanos Papanatsios <spapanatsios@gmail.com>
2019-11-19 23:45:30 +0200

    Added code to save grades and added error handling to AdminGrades page

--- src/pithia2/Views/AdminGrades.java
+++ src/pithia2/Views/AdminGrades.java
@@ -5,6 +5,7 @@ import java.awt.event.MouseEvent;
 import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -26,12 +27,16 @@ public class AdminGrades extends JFrame {
   private JButton HomeButton;
   private JButton SignoutButton;
   private JTable StudentTable;
-  private JButton ConfirmButton;
+  private JButton SaveButton;
   private JTable LessonTable;
   private JPanel TablePanel;
-  private JPanel ConfirmPanel;
+  private JPanel SavePanel;
   private JScrollPane Lessons;
   private JScrollPane Students;
+  private JLabel ErrorLabel;
+  private JPanel ErrorPanel;
+  private Student selectedStudent;
+  private Registration lastRegistration;
 
   AdminGrades() {
     add(RootPanel);
@@ -66,6 +71,8 @@ public class AdminGrades extends JFrame {
         loadLessons();
       }
     });
+
+    SaveButton.addActionListener(e -> save());
   }
 
   private void createUIComponents() {
@@ -99,19 +106,21 @@ public class AdminGrades extends JFrame {
     DefaultTableModel lessonTableModel = (DefaultTableModel) LessonTable.getModel();
     lessonTableModel.setRowCount(0);
 
+    selectedStudent = null;
+    lastRegistration = null;
+
     int studentTableSelectedRow = StudentTable.getSelectedRow();
     int studentCode = Integer.parseInt(StudentTable.getValueAt(studentTableSelectedRow, 0).toString());
     List<User> users = University.getUniversityInstance().getUsers();
-    Student selectedStudent;
 
     for (User user : users) {
       if (user instanceof Student && ((Student) user).getStudentCode() == studentCode) {
         selectedStudent = (Student) user;
-
         int registrationCount = selectedStudent.getRegistrations().size();
+
         if (registrationCount > 0) {
           int lastRegistrationIndex = registrationCount - 1;
-          Registration lastRegistration = selectedStudent.getRegistrations().get(lastRegistrationIndex);
+          lastRegistration = selectedStudent.getRegistrations().get(lastRegistrationIndex);
           List<RegisteredLesson> registeredLessons = lastRegistration.getRegisteredLessons();
 
           for (RegisteredLesson registeredLesson : registeredLessons) {
@@ -123,6 +132,32 @@ public class AdminGrades extends JFrame {
             ((DefaultTableModel) LessonTable.getModel()).addRow(row);
           }
         }
+      }
+    }
+  }
+
+  private void save() {
+    if (selectedStudent == null) {
+      ErrorLabel.setText("Select a student first.");
+    }
+    else if (lastRegistration == null) {
+      ErrorLabel.setText("Student has no registrations.");
+    }
+    else {
+      List<RegisteredLesson> registeredLessons = lastRegistration.getRegisteredLessons();
+      int index = 0;
+      for (RegisteredLesson registeredLesson : registeredLessons) {
+        double grade = Double.parseDouble(LessonTable.getValueAt(index, 2).toString());
+
+        if (grade < 0) {
+          registeredLesson.setGrade(0);
+        } else if (grade > 10) {
+          registeredLesson.setGrade(10);
+        } else {
+          registeredLesson.setGrade(grade);
+        }
+
+        index++;
       }
     }
   }
