19d41636f52afc7342322c0211395cacf7a700a5
feedmycat <konstantangelou@gmail.com>
2019-12-19 20:14:10 +0200

    Added functionality for PrerequisiteTable

--- src/pithia2/Views/LessonManagement.java
+++ src/pithia2/Views/LessonManagement.java
@@ -1,6 +1,9 @@
 package pithia2.Views;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.List;
+import java.util.Vector;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -22,19 +25,21 @@ public class LessonManagement extends JFrame {
   private JButton BackButton;
   private JButton SignoutButton;
   private JButton HomeButton;
-  private JPanel LessonsPanel;
+  private JPanel LessonPanel;
   private JScrollPane Lessons;
-  private JTable LessonsTable;
+  private JTable LessonTable;
   private JPanel MiscPanel;
   private JButton SaveButton;
   private JButton NewButton;
   private JButton DeleteButton;
   private JComboBox<Object> DepartmentsDropdown;
-  private JPanel PrerequisitesPanel;
-  private JTable PrerequisitesTable;
-  private JScrollPane AvailablePrerequisites;
-  private JTable AvailablePrerequisitesTable;
+  private JTable PrerequisiteTable;
   private JScrollPane Prerequisites;
+  private JPanel PrerequisitePanel;
+  private JScrollPane AvailablePrerequisites;
+  private JTable AvailablePrerequisiteTable;
+
+  private List<Lesson> lessons;
 
   LessonManagement() {
     add(RootPanel);
@@ -55,7 +60,7 @@ public class LessonManagement extends JFrame {
       dispose();
     });
 
-    DepartmentsDropdown.addActionListener(e -> addRows());
+    DepartmentsDropdown.addActionListener(e -> loadLessons());
 
     SignoutButton.addActionListener(e -> {
       Login login = new Login();
@@ -63,12 +68,54 @@ public class LessonManagement extends JFrame {
       dispose();
       Administrator.getAdminInstance().logout();
     });
+
+    LessonTable.addMouseListener(new MouseAdapter() {
+      @Override
+      public void mouseClicked(MouseEvent mouseEvent) {
+        super.mouseClicked(mouseEvent);
+        loadPrerequisites();
+      }
+    });
+
+    PrerequisiteTable.addMouseListener(new MouseAdapter() {
+      @Override
+      public void mouseClicked(MouseEvent mouseEvent) {
+        super.mouseClicked(mouseEvent);
+        DefaultTableModel prerequisiteTableModel = (DefaultTableModel) PrerequisiteTable.getModel();
+        Vector<Object> selectedRow = (Vector<Object>) prerequisiteTableModel.getDataVector()
+            .elementAt(PrerequisiteTable.getSelectedRow());
+        ((DefaultTableModel) AvailablePrerequisiteTable.getModel()).addRow(selectedRow);
+        prerequisiteTableModel.removeRow(PrerequisiteTable.getSelectedRow());
+//        ErrorLabel.setVisible(false);
+      }
+    });
+
+    AvailablePrerequisiteTable.addMouseListener(new MouseAdapter() {
+      @Override
+      public void mouseClicked(MouseEvent mouseEvent) {
+        super.mouseClicked(mouseEvent);
+        DefaultTableModel availablePrerequisiteTableModel = (DefaultTableModel) AvailablePrerequisiteTable.getModel();
+        Vector<Object> selectedRow = (Vector<Object>) availablePrerequisiteTableModel.getDataVector()
+            .elementAt(AvailablePrerequisiteTable.getSelectedRow());
+        ((DefaultTableModel) PrerequisiteTable.getModel()).addRow(selectedRow);
+        availablePrerequisiteTableModel.removeRow(AvailablePrerequisiteTable.getSelectedRow());
+//        ErrorLabel.setVisible(false);
+      }
+    });
   }
 
   private void createUIComponents() {
-    String[] columns = {"ID", "Lesson", "Semester", "Lab Hours", "Theory Hours", "Credits", "Type"};
-    DefaultTableModel tableModel = new DefaultTableModel(columns, 0);
-    LessonsTable = new JTable(tableModel);
+    String[] lessonColumns = {"ID", "Lesson", "Semester", "Lab Hours", "Theory Hours", "Credits", "Type"};
+    DefaultTableModel lessonTableModel = new DefaultTableModel(lessonColumns, 0);
+    LessonTable = new JTable(lessonTableModel);
+
+    String[] prerequisiteColumns = {"ID", "Lesson", "Credits"};
+    DefaultTableModel prerequisiteTableModel = new DefaultTableModel(prerequisiteColumns, 0);
+    DefaultTableModel availablePrerequisiteTableModel = new DefaultTableModel(prerequisiteColumns, 0);
+    PrerequisiteTable = new JTable(prerequisiteTableModel);
+    AvailablePrerequisiteTable = new JTable(availablePrerequisiteTableModel);
+    PrerequisiteTable.setDefaultEditor(Object.class, null);
+    AvailablePrerequisiteTable.setDefaultEditor(Object.class, null);
 
     List<Department> departments = University.getUniversityInstance().getDepartments();
     DepartmentsDropdown = new JComboBox<>();
@@ -76,14 +123,14 @@ public class LessonManagement extends JFrame {
       DepartmentsDropdown.addItem(department.getName());
     }
 
-    addRows();
+    loadLessons();
   }
 
-  private void addRows() {
-    ((DefaultTableModel) LessonsTable.getModel()).setRowCount(0);
+  private void loadLessons() {
+    ((DefaultTableModel) LessonTable.getModel()).setRowCount(0);
     List<Department> departments = University.getUniversityInstance().getDepartments();
     Department selectedDepartment = departments.get(DepartmentsDropdown.getSelectedIndex());
-    List<Lesson> lessons = selectedDepartment.getLessons();
+    lessons = selectedDepartment.getLessons();
 
     Object[] rowData = new Object[7];
     for (Lesson lesson : lessons) {
@@ -94,7 +141,24 @@ public class LessonManagement extends JFrame {
       rowData[4] = lesson.getTheoryHours();
       rowData[5] = lesson.getCredit();
       rowData[6] = lesson.getType();
-      ((DefaultTableModel) LessonsTable.getModel()).addRow(rowData);
+      ((DefaultTableModel) LessonTable.getModel()).addRow(rowData);
+    }
+  }
+
+  private void loadPrerequisites() {
+    // Reset the tables
+    ((DefaultTableModel) PrerequisiteTable.getModel()).setRowCount(0);
+    ((DefaultTableModel) AvailablePrerequisiteTable.getModel()).setRowCount(0);
+
+    int selectedLessonIndex = LessonTable.getSelectedRow();
+    List<Lesson> requiredLessons = lessons.get(selectedLessonIndex).getRequiredLessons();
+    for (Lesson requiredLesson : requiredLessons) {
+      Object[] row = new Object[3];
+      row[0] = requiredLesson.getId();
+      row[1] = requiredLesson.getName();
+      row[2] = requiredLesson.getCredit();
+
+      ((DefaultTableModel) PrerequisiteTable.getModel()).addRow(row);
     }
   }
 }
