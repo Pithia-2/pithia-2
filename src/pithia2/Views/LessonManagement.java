dcab46c814fcef794533662bb62635298a858813
Stefanos Papanatsios <spapanatsios@gmail.com>
2019-12-19 19:13:03 +0200

    Added LessonManagement page - Added nav button to AdminInfo

--- src/pithia2/Views/LessonManagement.java
+++ src/pithia2/Views/LessonManagement.java
@@ -4,58 +4,78 @@ import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.WindowConstants;
 import javax.swing.table.DefaultTableModel;
 import pithia2.GlobalConstants;
+import pithia2.Models.Administrator;
 import pithia2.Models.Department;
 import pithia2.Models.Lesson;
 import pithia2.Models.University;
 
-public class LessonsList extends JFrame {
+public class LessonManagement extends JFrame {
 
-  private JPanel LessonsListPanel;
-  private JButton BackButton;
-  private JButton HomeButton;
-  private JButton SignoutButton;
-  private JTable LessonsTable;
-  private JScrollPane Lessons;
-  private JPanel ErrorPanel;
-  private JLabel ErrorLabel;
-  private JComboBox<String> DepartmentsDropdown;
-  private JPanel LessonsPanel;
+  private JPanel RootPanel;
   private JPanel Navbar;
+  private JButton BackButton;
+  private JButton SignoutButton;
+  private JButton HomeButton;
+  private JPanel LessonsPanel;
+  private JScrollPane Lessons;
+  private JTable LessonsTable;
+  private JPanel MiscPanel;
+  private JButton SaveButton;
+  private JButton NewButton;
+  private JButton DeleteButton;
+  private JComboBox<Object> DepartmentsDropdown;
+  private JPanel PrerequisitesPanel;
+  private JTable PrerequisitesTable;
+  private JScrollPane AvailablePrerequisites;
+  private JTable AvailablePrerequisitesTable;
+  private JScrollPane Prerequisites;
 
-  LessonsList() {
-    add(LessonsListPanel);
+  LessonManagement() {
+    add(RootPanel);
     setSize(GlobalConstants.FRAME_WIDTH, GlobalConstants.FRAME_HEIGHT);
     setResizable(false);
     setLocationRelativeTo(null);
     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
     BackButton.addActionListener(e -> {
+      AdminInfo adminInfo = new AdminInfo();
+      adminInfo.setVisible(true);
+      dispose();
+    });
+
+    HomeButton.addActionListener(e -> {
       Home home = new Home();
       home.setVisible(true);
       dispose();
     });
 
     DepartmentsDropdown.addActionListener(e -> addRows());
+
+    SignoutButton.addActionListener(e -> {
+      Login login = new Login();
+      login.setVisible(true);
+      dispose();
+      Administrator.getAdminInstance().logout();
+    });
   }
 
   private void createUIComponents() {
     String[] columns = {"ID", "Lesson", "Semester", "Lab Hours", "Theory Hours", "Credits", "Type"};
     DefaultTableModel tableModel = new DefaultTableModel(columns, 0);
     LessonsTable = new JTable(tableModel);
-    LessonsTable.setDefaultEditor(Object.class, null);
 
     List<Department> departments = University.getUniversityInstance().getDepartments();
     DepartmentsDropdown = new JComboBox<>();
     for (Department department : departments) {
       DepartmentsDropdown.addItem(department.getName());
     }
+
     addRows();
   }
 
